831d1e0c7e869dcda7330934a575be607aaaa95e
Shashikumar8341 <shashi.b8341@gmail.com>
2024-05-28 19:45:10 +0530

    Add files via upload

---
+++ SpringVersion_Control/src/main/java/springversion_control/springVersion.java
@@ -0,0 +1,11 @@
+package springversion_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springVersion {
+	public static void main(String[] args) {
+		SpringApplication.run(springVersion.class, args);
+	}
+}
